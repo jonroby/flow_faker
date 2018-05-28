78522aae90f20ca6485db803b477d8a3464c9fc3
jonroby <joncaroby@gmail.com>
2018-05-28 07:46:14 -0700

    create switch for types for primitives, arrays, and literals

--- createFakeProps.js
+++ createFakeProps.js
@@ -23,3 +23,68 @@ function createFakeProps() {
 
   //  return flowData;
 }
+
+function createFakeProp(prop) {
+  switch (changeCase.lowerCase(prop.name)) {
+    case "number":
+      return getNumber(prop);
+
+    case "string":
+      return getString(prop);
+
+    case "boolean":
+      return getBoolean(prop);
+
+    case "void":
+      return null; // undefined?
+
+    case "array":
+      return prop.elements.reduce((acc, element) => {
+        const fakeProp = createFakeProp(element);
+        return acc.concat(fakeProp);
+      }, []);
+
+    case "object":
+
+    case "union":
+      return prop.elements.map(p => {
+        return createFakeProp(p);
+      });
+
+    default:
+      console.log("No match.");
+  }
+}
+
+// TODO: regex on propName and use a corresponding faker function if match
+function getNumber(propName, prop) {
+  // TODO: regex on propName and use a corresponding faker function if match
+
+  return faker.random.number();
+}
+
+function getString(propName, prop) {
+  return faker.random.word();
+}
+
+function getBoolean(propName, prop) {
+  // TODO: Figure out a sensible way to store both true and false
+  return faker.random.boolean();
+}
+
+function getLiteralsAndUnion() {
+  // TODO: literals and union
+  // flowDoc.props['primitive'].flowType.name
+  // flowDoc.props['primitive'].flowType.elements
+  // [ { name: 'literal', value: '\'string\'' },
+  //   { name: 'literal', value: '\'otherstring\'' },
+  //   { name: 'number' } ]
+}
+
+function getArr() {}
+
+function getObj() {}
+
+createFakeProps();
+
+// module.exports = getFakeProps;
