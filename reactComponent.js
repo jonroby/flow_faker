d6dc3bc878614bf2bba1b8099aeda5c45312534e
jonroby <joncaroby@gmail.com>
2018-05-28 20:00:25 -0700

    try other examples

--- reactComponent.js
+++ reactComponent.js
@@ -8,9 +8,21 @@ type Props = {
   username: string,
   firstname: string,
   lastname: string,
+  isSubscriber: boolean,
   id: number,
   favoriteMovies: Array<string>,
   favoriteThings: Array<string | number>,
+  userType: "beginner" | "intermediate" | "expert",
+  litNum: 2,
+  litBool: true,
+  userData: {
+    thing1: number,
+    thing2: {
+      nested: string,
+      nested2: number,
+    },
+  },
+  getSomething: (name: string, isSubscriber: boolean) => number,
 };
 
 class User extends Component<Props> {
